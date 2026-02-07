1d4e6cb8cf1c86564938184ca7e47e1041f00d9f
haikeyi <4voacdo@gmail.com>
2026-02-07 16:10:28 +0800

    finished merge sort function in Node.cpp, to be added to main program logic

--- TestingDSA/TestingDSA/Game.h
+++ TestingDSA/TestingDSA/Game.h
@@ -1,7 +1,7 @@
 #pragma once
+#include <chrono>
 #include <iostream>
 #include <string>
-#include <chrono>
 using namespace std;
 
 // Group 8 Team Members
@@ -9,51 +9,53 @@ using namespace std;
 // Winston Chin, S10266664
 // Asher Ng, S10267475
 
-class Game
-{
+class Game {
 private:
-	string name;
-	int minPlayers;
-	int maxPlayers;
-	int minTime;
-	int maxTime;
-	int year;
-	int rating;
-	bool isBorrowed;
-	int borrowDate; //stored as YYYYMMDD
-	int returnDate; //-1 if not returned yet, else store actual date as YYYYMMDD
-	Game* next;
+  string name;
+  int minPlayers;
+  int maxPlayers;
+  int minTime;
+  int maxTime;
+  int year;
+  int rating;
+  bool isBorrowed;
+  int borrowDate; // stored as YYYYMMDD
+  int returnDate; //-1 if not returned yet, else store actual date as YYYYMMDD
+  Game *next;
 
 public:
-	// constructor
-	Game();
-	Game(string name, int minPlayers, int maxPlayers, int minTime, int maxTime, int year, bool isBorrowed, int returnDate);
-	
-	//  get and set
-	void setName(string name);
-	string getName();
-	void setMinPlayers(int minPlayers);
-	int getMinPlayers();
-	void setMaxPlayers(int maxPlayers);
-	int getMaxPlayers();
-	void setMinTime(int minTime);
-	int getMinTime();
-	void setMaxTime(int maxTime);
-	int getMaxTime();
-	void setYear(int year);
-	int getYear();
-	void setIsBorrowed(bool isBorrowed);
-	bool getIsBorrowed();
-	void setBorrowDate(int borrowDate);
-	int getBorrowDate();
-	void setReturnDate(int returnDate);
-	int getReturnDate();
+  // constructor
+  Game();
+  Game(string name, int minPlayers, int maxPlayers, int minTime, int maxTime,
+       int year, bool isBorrowed, int returnDate);
 
-	// calculates the number of days a game is borrowed from today's date
-	// returns -1(if not borrowed) or the number of days between today's date and borrowDate
-	int borrowedDays();
+  //  get and set
+  void setName(string name);
+  string getName();
+  void setMinPlayers(int minPlayers);
+  int getMinPlayers();
+  void setMaxPlayers(int maxPlayers);
+  int getMaxPlayers();
+  void setMinTime(int minTime);
+  int getMinTime();
+  void setMaxTime(int maxTime);
+  int getMaxTime();
+  void setYear(int year);
+  int getYear();
+  void setIsBorrowed(bool isBorrowed);
+  bool getIsBorrowed();
+  void setBorrowDate(int borrowDate);
+  int getBorrowDate();
+  void setReturnDate(int returnDate);
+  int getReturnDate();
 
-	// calculates the number of days a game is returned from today's date
-	// returns -1(if not returned) or the number of days between today's date and returnDate 
-	int returnedDays();
+  // calculates the number of days a game is borrowed from today's date
+  // returns -1(if not borrowed) or the number of days between today's date and
+  // borrowDate
+  int borrowedDays();
+
+  // calculates the number of days a game is returned from today's date
+  // returns -1(if not returned) or the number of days between today's date and
+  // returnDate
+  int returnedDays();
 };
--- TestingDSA/TestingDSA/Node.cpp
+++ TestingDSA/TestingDSA/Node.cpp
@@ -1,23 +1,23 @@
-#include "Game.h"
 #include <iostream>
 using namespace std;
 
 class Node {
 public:
-  Game data;
+  int data;
   Node *next;
-  Node(Game game) {
-    data = game;
+  Node(int value) {
+    data = value;
     next = nullptr;
   }
 };
 
 // Function to split the linked list into two halves
 // using the fast and slow node pointers to find the pointer
-// at the halfway mark of the linked list
+// and splits the linked list at the centre,
+// and returning the pointer to the second half of the linked list
 // Note: the linked list is a singly linked list
 // i.e. there is no branching in the linked list
-
+// the split function splits the linked list
 Node *split(Node *head) {
   // first set both fast and slow node to head
   Node *fast = head;
@@ -42,4 +42,74 @@ Node *split(Node *head) {
   slow->next = nullptr;
   // return temp, which is the second list
   return temp;
+}
+
+// Function to merge two sorted halves of a linked list together
+Node *merge(Node *first, Node *second) {
+  // if either list is empty, return the other list
+  if (first == nullptr)
+    return second;
+  if (second == nullptr)
+    return first;
+
+  // pick the smaller value between the first and second nodes
+  // TODO: modify to sort by properties of game
+  if (first->data < second->data) {
+    // recursively merge the rest of the lists and
+    // link the result to the current node
+    first->next = merge(first->next, second);
+    return first;
+  } else {
+    // recursively merge the rest of the lists
+    // and link the result to the current node as well
+    second->next = merge(first, second->next);
+    return second;
+  }
+}
+
+// Function to perform merge sort on a linked list
+Node *mergeSort(Node *head) {
+  // Base case:
+  // if the list is empty or has only one node,
+  // it's already sorted and does not require any more sorting
+  if (head == nullptr || head->next == nullptr) {
+    return head;
+  }
+
+  // split the list into two halves
+  Node *second = split(head);
+
+  // recursively sort each half
+  head = mergeSort(head);
+  second = mergeSort(second);
+
+  // merge the two sorted halves
+  return merge(head, second);
+}
+
+// Function to print out the list
+void printList(Node *head) {
+  Node *current = head;
+  while (current != nullptr) {
+    cout << current->data << " ";
+    if (current->next) {
+      cout << "-> ";
+    }
+    current = current->next;
+  }
+  cout << endl;
+}
+
+int main() {
+  // create hard coded linked list
+  Node *head = new Node(9);
+  head->next = new Node(8);
+  head->next->next = new Node(5);
+  head->next->next->next = new Node(2);
+  head->next->next->next->next = new Node(7);
+
+  head = mergeSort(head);
+  printList(head);
+
+  return 0;
 }
